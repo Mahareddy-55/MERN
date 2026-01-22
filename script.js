7f54fdd56e8866f9543bd5b3c091ed677cfb19cf
mahathi <yeddulamahathi65@gmail.com>
2026-01-22 15:57:21 +0530

    JS basics and  ES6 feature started

---
+++ script.js
@@ -0,0 +1,107 @@
+// console.log("Hello world!");
+
+// var a=10;
+// console.log(a);
+
+// let b=20;
+// console.log(b);
+
+// const c=30;
+
+// console.log(c);
+
+// var a=10;
+// var str = "maha";
+// // var bool = true;
+// var un;
+// var n = null;
+// var big = 9007199254740991n;
+// var sym = Symbol("id");
+
+// console.log(typeof a);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof n);
+// console.log(typeof big);
+// console.log(typeof sym);
+
+
+// var a=10;
+// var b="20";
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// var a=10;
+// var b=20;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// var a=10;
+// var b="10";
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+
+
+
+// a=true;
+// b=false;
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+
+
+// var  arr=[10,20,30,40,50];
+// console.log(arr);
+// console.log(typeof arr);
+
+// var obj = {
+//     name: "Maha",
+//     age: 20}
+// console.log(obj);
+// console.log(typeof obj);
+
+
+// let i=1;
+// while(i<=10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// let i=1;
+// do{
+//     console.log('do.... while');
+// }
+// while(i===0);
+
+
+// let marks = 70;
+// if(marks>=90)
+//     console.log("0 grade");
+// else if(marks>=80)
+//     console.log("A grade");
+// else if(marks>=70)
+//     console.log("B grade");
+// else
+//     console.log("C grade");
+
+// i=8;
+// let result = (i%2==0)?"even":"odd";
+// console.log(result);
+
+
+var [m1,m2,m3]=[10,20,30];
+console.log(m1);
+console.log(m2);
+console.log(m3);
